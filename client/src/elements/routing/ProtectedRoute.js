32f210bb2890c8f1e130c117eb3e2b970b18767b
NhatTam <19110283@student.hcmute.edu.vn>
2022-07-05 16:32:49 +0700

    Done

---
+++ client/src/elements/routing/ProtectedRoute.js
@@ -0,0 +1,29 @@
+import { Navigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
+import Spinner from 'react-bootstrap/Spinner'
+import NavbarMenu from '../layout/NavbarMenu'
+
+const ProtectedRoute = ({ children }) => {
+    const { authState: { authLoading, isAuthenticated } } = useContext(AuthContext)
+
+    if (authLoading) {
+        return (
+            <div className='spinner-container'>
+                <Spinner animation='border' variant='info' />
+            </div>
+        )
+    }
+    if (isAuthenticated) {
+        return (
+            <>
+                <NavbarMenu />
+                {children}
+            </>
+        )
+    } else {
+        return <Navigate to='/login' />
+    }
+}
+
+export default ProtectedRoute
